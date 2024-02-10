960d62528329135651fc2ab0dddbe1bd45e7e297
WinterDinder <115167157+WinterDinder@users.noreply.github.com>
2024-02-10 20:22:24 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function changeColors() {
+    var body = document.body;
+  
+    
+    if (body.style.backgroundColor === 'black') {
+      body.style.backgroundColor = 'white';
+      body.style.color = 'black';
+    } else {
+      body.style.backgroundColor = 'black';
+      body.style.color = 'white';
+    }
+  }
+  
